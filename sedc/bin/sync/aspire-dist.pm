5c5ab1d21fd89e0cf0cc802c76e0fa907db55b34
moses373 <moses373@gmail.com>
2016-01-29 11:53:15 -0700

    removed private data

--- sedc/bin/sync/aspire-dist.pm
+++ sedc/bin/sync/aspire-dist.pm
@@ -13,3 +13,21 @@ This perl module is for connecting to aspire databases.
 #getTeachers
 
 1;
+#db connection = return the DBH
+#sybase
+#getDBH
+#205.125.67.238 1443 kohaadmin tr8t0r
+package Sync::Aspire;
+
+use Modern::Perl;
+use DBI;
+sub getDBH()
+{
+    my $host = "";
+    my $user = "";
+    my $passwd = "";
+    my $db = "";
+}
+
+my $dbh = DBI ->connect("dbi:Sybase:server=$host, port=1443, $user, $passwd, database=$db");
+return $dbh;
